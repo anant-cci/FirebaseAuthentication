331731e1e81020b0f249bb0dc4c58755f197260d
Anant Kannaik <anant.kannaik@creativecapsule.com>
2018-01-18 12:19:29 +0530

    Pod install

--- FirebaseAuthentication/AppDelegate.swift
+++ FirebaseAuthentication/AppDelegate.swift
@@ -7,16 +7,42 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        let authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+        
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(),
+            ]
+        authUI?.providers = providers
+        
+        let authViewController = authUI!.authViewController()
+        window?.rootViewController = authViewController
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -41,6 +67,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseAuthentication' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseAuthentication
+
+pod 'FirebaseUI/Auth'
+pod 'FirebaseUI/Google'
+
+end
